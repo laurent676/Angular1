9dabfccc4331943497c0ac7fe6a71050404022c2
laurent676 <57151750+laurent676@users.noreply.github.com>
2019-11-12 21:14:08 +0100

    Configuration serveur node.js + mise en route serveur + déploiement application gestion des appareil ménager

---
+++ src/app/mon-premier2/mon-premier2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mon-premier2',
+  templateUrl: './mon-premier2.component.html',
+  styleUrls: ['./mon-premier2.component.scss']
+})
+export class MonPremier2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
